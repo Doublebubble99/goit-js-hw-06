a7b9434bf8bd61bd5c3037f2c6b785a42bc98d3a
Artemon Chik <zagrousaspear@gmail.com>
2023-02-25 10:33:27 +0100

    Complete home-work 6

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const createButton = document.querySelector("[data-create]");
+// const destroyButton = document.querySelector("[data-destroy]");
+// const input = document.querySelector("input");
+// const boxes = document.querySelector("#boxes");
+// createButton.addEventListener("click", () => {
+//   createBoxes(input.value);
+// });
+// destroyButton.addEventListener("click", () => {
+//   destroyBoxes();
+// });
+
+// function createBoxes(amount) {
+//   const box = "<div class = 'box'></div>";
+//   const boxArray = [];
+//   for (let v = 0; v < amount; v += 1) {
+//     boxArray.push(box);
+//   }
+//   const boxList = boxArray
+//     .map((array) => boxes.insertAdjacentHTML("afterbegin", array))
+//     .join("");
+//   boxList.s;
+// }
+// function destroyBoxes() {
+//   boxes.innerHTML = "";
+// }
